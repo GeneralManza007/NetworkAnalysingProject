bd20f1f3bfac41e9e11a64baae03518f0fe79b1d
Hamza Malik <ZLAC357@live.rhul.ac.uk>
2024-12-25 19:33:42 +0000

    #1 Initalising Project

---
+++ NetworkAnalyser/src/main/java/com/github/GeneralManza007/NetworkAnalyser/NetworkAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.github.GeneralManza007.NetworkAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkAnalyserApplication.class, args);
+	}
+
+}
