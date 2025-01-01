dbe8e3482b2f1797a619ca8a8094940a35ddfd25
Hamza Malik <ZLAC357@live.rhul.ac.uk>
2025-01-01 15:55:06 +0000

    #9 Creating RestController and Methods

--- NetworkAnalyser/src/main/java/com/github/GeneralManza007/NetworkAnalyser/controller/PacketLogController.java
+++ NetworkAnalyser/src/main/java/com/github/GeneralManza007/NetworkAnalyser/controller/PacketLogController.java
@@ -1,5 +1,48 @@
 package com.github.GeneralManza007.NetworkAnalyser.controller;
 
-public class PacketLogController {
+import org.springframework.web.bind.annotation.*;
+import com.github.GeneralManza007.NetworkAnalyser.model.PacketLog;
+import com.github.GeneralManza007.NetworkAnalyser.repositories.PacketLogRepository;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/packets")
+public class PacketLogController {
+	
+	private final PacketLogRepository repository;
+	
+	public PacketLogController(PacketLogRepository repository) {
+		this.repository = repository;
+	}
+	
+	@GetMapping
+	public Iterable<PacketLog>getAllPackets(){
+		return repository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<PacketLog>getPacketById(@PathVariable Long id){
+		return repository.findById(id);
+	}
+	
+	@PostMapping
+	public PacketLog createPacket(@RequestBody PacketLog packetLog) {
+		return repository.save(packetLog);
+	}
+	
+	@PutMapping
+	public PacketLog updatePacket(@PathVariable Long id, @RequestBody PacketLog packetLog) {
+		
+		if(repository.existsById(id)) {
+			packetLog.setId(id);
+			return repository.save(packetLog);
+		}
+		throw new RuntimeException("Packet not found with id + " + id );
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletePacket(@PathVariable Long id){
+		repository.deleteById(id);
+	}
 }
